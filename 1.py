65d6029faf28e658bde7e16316d35270354a295c
Grisuno <grisuno@gmail.com>
2024-06-27 22:57:41 -0400

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,44 @@
+# Script 1: Introducción a Python
+
+# Este es el primer script de una serie de tutoriales en Python.
+# En este script, cubriremos los conceptos básicos de Python, como imprimir en la consola, 
+# comentarios, y variables.
+
+# Importar la librería sys para obtener información del sistema
+import sys
+
+# Imprimir en la consola
+print("¡Hola, mundo!")  # Esto imprimirá "¡Hola, mundo!" en la consola.
+
+# Comentarios en Python
+# Este es un comentario de una sola línea.
+"""
+Este es un comentario
+de múltiples líneas.
+"""
+
+# Variables
+# Una variable es un espacio de memoria donde almacenamos datos.
+# Python es un lenguaje de tipado dinámico, lo que significa que no necesitamos especificar
+# el tipo de variable al declararla.
+
+# Asignación de variables
+mensaje = "¡Bienvenido a Python!"
+numero_entero = 10
+numero_flotante = 3.14
+booleano = True
+
+# Imprimir variables
+print(mensaje)  # Esto imprimirá el valor de la variable mensaje.
+print(numero_entero)  # Esto imprimirá el valor de la variable numero_entero.
+print(numero_flotante)  # Esto imprimirá el valor de la variable numero_flotante.
+print(booleano)  # Esto imprimirá el valor de la variable booleano.
+
+# Verificar el tipo de variable
+print(type(mensaje))  # Esto imprimirá <class 'str'>
+print(type(numero_entero))  # Esto imprimirá <class 'int'>
+print(type(numero_flotante))  # Esto imprimirá <class 'float'>
+print(type(booleano))  # Esto imprimirá <class 'bool'>
+
+# Este es el final del primer script. Continúa con el siguiente script para aprender más
+# sobre Python.
